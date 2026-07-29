f81f288f1bf9bf844e65f4b344195dd20652f8d4
nasirhajiyani <64406890+nasirhajiyani@users.noreply.github.com>
2026-07-29 16:38:14 +0100

    Development for home page and Level 2
    Also includes basic instructions for the player

---
+++ Assets/Scripts/Instriction/gravity_Intruction.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+
+
+
+
+public class Gravity_Intruction : MonoBehaviour
+{
+    public GameObject textIntruction;
+    public GameObject intructionTrigger;
+
+    public Rigidbody2D rb;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textIntruction.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+       
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {   
+        if(collision.TryGetComponent(out Player player))
+        {
+            
+            textIntruction.SetActive(true);
+            StartCoroutine(GravityIntruction());
+   
+            
+        }
+        
+    }
+
+    private IEnumerator GravityIntruction()
+    {
+        
+        yield return new WaitForSeconds(5f);
+        textIntruction.SetActive(false);
+    }
+}
